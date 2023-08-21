2c462873140aa7145dc2b90050c0af7ae9343433
0x1eef <0x1eef@protonmail.com>
2023-08-21 16:37:10 -0300

    Add "ls --help" testcase

--- test/readme_examples_test.rb
+++ test/readme_examples_test.rb
@@ -10,6 +10,17 @@ class READMETest < Test::Unit::TestCase
                  run_example("ls --path test/fakefs/ --grep [^.gitkeep]").stdout
   end
 
+  def test_ls_command_help
+    help = "Usage: ls [OPTIONS]\n\n" \
+           "Description:\n" \
+           "  Lists the contents of a directory\n\n" \
+           "Options: \n" \
+           "  -h, --help                 Show help\n" \
+           "  -g, --grep PATTERN         A regular expression\n" \
+           "  -p, --path PATH            The target path\n\n"
+    assert_equal help, run_example("ls --help").stdout
+  end
+
   private
 
   def run_example(command)
