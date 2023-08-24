24ea618b726d81ef6584fad2bfe78a6d8c356793
0x1eef <0x1eef@protonmail.com>
2023-08-24 17:42:31 -0300

    Add API documentation (Cmd::OptionParser)

--- lib/cmd/mixins/option_parser.rb
+++ lib/cmd/mixins/option_parser.rb
@@ -3,25 +3,68 @@
 require "optparse"
 require "ryo"
 
+##
+# The {Cmd::OptionParser Cmd::OptionParser} module provides a light wrapper
+# around Ruby's
+# [OptionParser](https://docs.ruby-lang.org/en/3.2/OptionParser.html)
+# class. The {Cmd::OptionParser::ClassMethods} module provides class methods
+# available to a class who include this module, and the
+# {Cmd::OptionParser Cmd::OptionParser}
+# modules provides instance methods available to a class who include this
+# module.
 module Cmd::OptionParser
   def self.included(klass)
     klass.extend(ClassMethods)
   end
 
   module ClassMethods
+    ##
+    # Set command banner.
+    #
+    # @param [String] usage
+    #  An example of how a command is used.
+    #
+    # @param [String] description
+    #  The description of a command.
+    #
+    # @return [void]
     def set_banner(usage:, description:)
       banner.usage = usage
       banner.description = description
     end
 
+    ##
+    # Set a command option.
+    #
+    # @param [String] short
+    #  Short option switch.
+    #
+    # @param [String] long
+    #  Long option switch.
+    #
+    # @param [String] desc
+    #  The description of an option.
+    #
+    # @param [Class] as
+    #  The type (String, Integer, etc) of the option's value.
+    #
+    # @return [void]
     def set_option(short, long, desc, as: String)
       option_parser.on(short, long, desc, as)
     end
 
+    ##
+    # Set command option defaults.
+    #
+    # @param [{String => Object}] defaults
+    #
+    # @return [void]
     def set_default(defaults)
       @defaults = Ryo.from(defaults)
     end
 
+    ##
+    # (see Cmd::OptionParser#option_parser)
     def option_parser
       @option_parser ||= ::OptionParser.new(nil, 26, " " * 2).tap do
         _1.banner = ""
@@ -29,27 +72,53 @@ module Cmd::OptionParser
       end
     end
 
+    ##
+    # @private
     def banner
       @banner ||= Ryo({})
     end
 
+    ##
+    # @private
     def defaults
-      @defaults || {}
+      @defaults ||= Ryo({})
     end
   end
 
+  ##
+  # Parses an array of strings with
+  # [OptionParser#parse](https://docs.ruby-lang.org/en/3.2/OptionParser.html#method-i-parse).
+  #
+  # @param [Array<String>] argv
+  #  An array of strings
+  #
+  # @return [Ryo::Object]
+  #  Returns the parsed options as a Ryo object
   def parse_options(argv)
     options = Ryo.from(self.class.defaults)
     option_parser.parse(argv, into: options)
     options
   end
 
+  ##
+  # Parses an array of strings with
+  # [OptionParser#parse!](https://docs.ruby-lang.org/en/3.2/OptionParser.html#method-i-parse-21).
+  #
+  # @param [Array<String>] argv
+  #  An array of strings
+  #
+  # @return [Ryo::Object]
+  #  Returns the parsed options as a Ryo object
   def parse_options!(argv)
     options = Ryo.from(self.class.defaults)
     option_parser.parse!(argv, into: options)
     options
   end
 
+  ##
+  # @return [OptionParser]
+  #  Returns an instance of
+  #  [OptionParser](https://docs.ruby-lang.org/en/3.2/OptionParser.html)
   def option_parser
     self.class.option_parser
   end
