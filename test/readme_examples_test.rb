b760fe3dfa681de1fbd4b86bf4e1651a3c364358
0x1eef <0x1eef@protonmail.com>
2023-10-01 18:07:44 -0300

    Update test

--- test/readme_examples_test.rb
+++ test/readme_examples_test.rb
@@ -7,7 +7,7 @@ class READMETest < Test::Unit::TestCase
 
   def test_ls_command
     assert_equal "dev\netc\nhome\n",
-                 run_example("ls --path test/fakefs/ --grep [^.gitkeep]").stdout
+                 run_example("ls --directory test/fakefs/ --grep [^.gitkeep]").stdout
   end
 
   def test_ls_command_help
@@ -17,7 +17,7 @@ class READMETest < Test::Unit::TestCase
            "Options: \n" \
            "  -h, --help                 Show help\n" \
            "  -g, --grep PATTERN         A regular expression\n" \
-           "  -p, --path PATH            The target path\n\n"
+           "  -d, --directory PATH       A path to a directory\n\n"
     assert_equal help, run_example("ls --help").stdout
   end
 
